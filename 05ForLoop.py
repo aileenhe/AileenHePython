66a3e782f874f479be2a6ec032da7c52ac729621
aileenhe <aileen.he@education.nsw.gov.au>
2021-10-22 09:50:15 +1100

    for loop with box

--- 05ForLoop.py
+++ 05ForLoop.py
@@ -1,2 +1,9 @@
+print(" ------------------------------------------------") 
+print("|                                                |")
+print("|    05ForLoop                                   |")
+print("|    Name : Aileen He                            |") 
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
 for x in range(1,500):
   print(x)
