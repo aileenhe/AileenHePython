afdd69c67405d2a0cb54c79cc5cc20940d3d1bb8
aileenhe <aileen.he@education.nsw.gov.au>
2021-10-27 12:10:41 +1100

    06 done

---
+++ 06whileLoop.py
@@ -0,0 +1,15 @@
+print(" ------------------------------------------------") 
+print("|                                                |")
+print("|    06whileLoop                                 |")
+print("|    Name : Aileen He                            |") 
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+ans = input("What is the name of this class? ")
+while ans != ("IST"):
+    print("Not correct - try again")
+    ans = input("What is the name of this class? ")
+    continue
+else:
+    print("\nCongratualtions!!")
+
