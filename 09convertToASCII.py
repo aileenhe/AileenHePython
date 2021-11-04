dcb28f458f47be07961030e5585669547d841402
aileenhe <aileen.he@education.nsw.gov.au>
2021-11-05 09:21:58 +1100

    convert09

---
+++ 09convertToASCII.py
@@ -0,0 +1,9 @@
+
+
+str1 = input ("What is your string? ")
+ascii_values = []
+equal_sign = ('=')
+for character in str1:
+    ascii_values.append(ord(character))
+for i in range(len(str1)): 
+    print(str1[i],equal_sign,ascii_values[i], sep="")
